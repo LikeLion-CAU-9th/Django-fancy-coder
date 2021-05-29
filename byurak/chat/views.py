b3b928198eea46f00c3ebd4d5a9ad80769bcd1a9
oereo <dlstpgns0406@gmail.com>
2021-05-30 00:14:40 +0900

    feat: add index, room method in views.py

--- byurak/chat/views.py
+++ byurak/chat/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
-# Create your views here.
+from chat.models import ChattingRoom
+
+
+def index(request):
+    chat_room = ChattingRoom.objects.all()
+    chat_room_index = {}
+    chat_room_member = {}
+    for index in range(0, chat_room.count()):
+        chat_room_no = chat_room[index].chat_id
+        chat_room_mem = chat_room[index].room
+        try:
+            chat_room_index[chat_room_no] += 1
+            chat_room_member[chat_room_no] += chat_room_mem
+        except:
+            chat_room_index[chat_room_no] = 1
+
+    return render(request, 'chat/index.html', {'chat_room': chat_room_index, 'chat_member': chat_room_member})
+
+
+def room(request, room_name):
+    return render(request, 'chat/room.html', {'room_name': room_name})
