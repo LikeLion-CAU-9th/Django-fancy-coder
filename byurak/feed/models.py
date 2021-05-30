004b9b33cd6f534656a3df3a8cc3e34b8aae25f2
JeongH0 <alswjdgh5095@gmail.com>
2021-05-31 02:17:11 +0900

    feat: add community html files

--- byurak/feed/models.py
+++ byurak/feed/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from uuid import uuid4
+from datetime import datetime
+import os
+
+def uuid_name_upload_to(instance, filename):
+    ymd_path = datetime.now().strftime('%Y/%m/%d')  # 업로드하는 년/월/일 별로
+    uuid_name = uuid4().hex
+    extension = os.path.splitext(filename)[-1].lower()  # 확장자 추출하고, 소문자로 변환
+    return '/'.join([
+        ymd_path,
+        uuid_name[:2],
+        uuid_name + extension,
+    ])
 
 # Create your models here.
+class Board(models.Model):
+    host=models.ForeignKey(User,related_name="host", on_delete=models.CASCADE, blank=True, null=True)
+    title=models.CharField(max_length=100)
+    image=models.ImageField(upload_to=uuid_name_upload_to, blank=True, null=True)
+    body=models.TextField(help_text="본문을 입력하세요")
+    created_at=models.DateField(auto_now_add=True)
+
--- byurak/feed/urls.py
+++ byurak/feed/urls.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 from django.urls import path
-from feed.views import service_landing
+from .views import *
 
+app_name = 'feed'
 
 urlpatterns = [
     path('', service_landing, name="landing"),
+    path('home', board_home, name="board_home"),
+    path('create/', board_create, name="board_create"),
+    path('update/<int:pk>', board_update, name="board_update"),
+    path('delete/<int:pk>', board_delete, name="board_delete"),
+    path('list/', board_list, name="board_list"),
+    path('detail/<int:pk>', board_detail, name="board_detail"),
 ]
 
