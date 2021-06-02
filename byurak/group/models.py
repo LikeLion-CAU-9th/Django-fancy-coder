de840181304eab497676a3fc685374c7c5e276e7
oereo <dlstpgns0406@gmail.com>
2021-06-02 17:27:05 +0900

    feat: add Group model class

--- byurak/group/models.py
+++ byurak/group/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
-# Create your models here.
+"""
+Implementation List
+1. Group class
+2. Group Community class
+
+"""
+
+
+class Group(models.Model):
+    users = models.JSONField()
+    mento_users = models.JSONField()
+    mentee_users = models.JSONField()
+    representive = models.ForeignKey(User, on_delete=models.CASCADE, help_text='유저')  
+    start_date = models.DateTimeField(
+        default=timezone.now,
+        blank=True,
+        null=True,
+        verbose_name='그룹 시작일'
+    )
+    end_date = models.DateTimeField(
+        default=timezone.now + datetime.timedelta(days=1),
+        blank=True,
+        null=True,
+        verbose_name='그룹 시작일'
+    )
+    keyword = models.CharField(max_length=63, null=True, blank=True)
+    short_description = models.CharField(max_length=255, null=True, blank=True)
+     
