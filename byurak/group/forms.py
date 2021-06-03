cd02ce93b8441534295507bfdea1769e34dcfa4d
oereo <dlstpgns0406@gmail.com>
2021-06-04 02:28:58 +0900

    feat: add GroupNoticeForm class

---
+++ byurak/group/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from group.models import GroupNotice
+
+class GroupNoticeForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'body')
+
+    def validate_title(self):
+        title = self.cleaned_data.get('title')
+
+        if title is None:
+            raise forms.ValidationError('Title is None.')
+        else:
+            return title
+
+    def validate_body(self):
+        body = self.cleaned_data.get('body')
+
+        if body is None:
+            raise forms.ValidationError('Body is None.')
+        else:
+            return body
