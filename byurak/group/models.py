d4e62ebf2795d95489fc715e97eb8fa6688f29de
oereo <dlstpgns0406@gmail.com>
2021-06-03 16:11:32 +0900

    refactor: fix group field format and add title field

--- byurak/group/models.py
+++ byurak/group/models.py
@@ -12,7 +12,7 @@ Implementation List
 
 
 class Group(models.Model):
-    
+
     COMPLETE = 'complete'
     RECRUITMENT = 'recruitment'
     OVERDUE = 'overdue'
@@ -22,12 +22,16 @@ class Group(models.Model):
         (RECRUITMENT, 'RECRUITMENT'),
         (OVERDUE, 'OVERDUE')
     ]
+    title = models.CharField(max_length=30, null=True, blank=True)
     users = models.CharField(max_length=255, null=True, blank=True)
     mento_users = models.CharField(max_length=255, null=True, blank=True)
     mentee_users = models.CharField(max_length=255, null=True, blank=True)
-    limited_user_numbers = models.IntegerField(default=1, null=True, blank=True)
-    status = models.CharField(max_length=31, choices=STATUS_TYPES, default=RECRUITMENT, help_text='그룹 상태')
-    representive = models.ForeignKey(User, on_delete=models.CASCADE, help_text='유저')  
+    limited_user_numbers = models.IntegerField(
+        default=1, null=True, blank=True)
+    status = models.CharField(
+        max_length=31, choices=STATUS_TYPES, default=RECRUITMENT, help_text='그룹 상태')
+    representive = models.ForeignKey(
+        User, on_delete=models.CASCADE, help_text='유저')
     start_date = models.DateTimeField(
         default=timezone.now,
         blank=True,
@@ -42,7 +46,31 @@ class Group(models.Model):
     )
     keyword = models.CharField(max_length=63, null=True, blank=True)
     short_description = models.CharField(max_length=255, null=True, blank=True)
-     
+
+    def __str__(self):
+        return self.title
+
+    @staticmethod
+    def get_splited_users(users):
+        return users.split(',')
+
+    @property
+    def get_users(request):
+        users_information = []
+        users = self.get_splited_users(self.users)
+
+        for user_id in users:
+            try:
+                user = User.objects.get(id=user_id)
+            except UserDoesNotExist:
+                user = ''
+            users_information.append(user)
+        return users_information
+
+    @property
+    def get_mento_users(request):
+        return self.mento_users.split(',')
+    
 
 class GroupNotice(models.Model):
     group = models.ForeignKey(Group, on_delete=models.CASCADE, help_text='그룹')
