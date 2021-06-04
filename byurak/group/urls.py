2a72bb79d040356cf08580e6cdc745ed77fdc6f2
oereo <dlstpgns0406@gmail.com>
2021-06-04 14:53:51 +0900

    feat: add GroupNoticeAPIView urls

--- byurak/group/urls.py
+++ byurak/group/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from group.views import group_detail, group_list
+from group.views import GroupNoticeAPIView, group_list
 
 
 urlpatterns = [
-    path('<int:pk>', group_detail, name="groupDetail"),
+    path('<int:pk>', GroupNoticeAPIView.as_view(), name="groupDetail"),
     path('', group_list, name="groupList"),
 ]
