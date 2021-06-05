c789a42058e180e46da3f314ed69b11d4141fdc9
oereo <dlstpgns0406@gmail.com>
2021-06-05 16:01:30 +0900

    feat: add GroupCalendarForm class

--- byurak/group/forms.py
+++ byurak/group/forms.py
@@ -1,9 +1,31 @@
 from django import forms
-from group.models import GroupNotice
+from group.models import GroupNotice, GroupCalendar
 
 class GroupNoticeForm(forms.ModelForm):
     class Meta:
         model = GroupNotice
+        fields = ('title', 'body')
+
+    def validate_title(self):
+        title = self.cleaned_data.get('title')
+
+        if title is None:
+            raise forms.ValidationError('Title is None.')
+        else:
+            return title
+
+    def validate_body(self):
+        body = self.cleaned_data.get('body')
+
+        if body is None:
+            raise forms.ValidationError('Body is None.')
+        else:
+            return body
+
+
+class GroupCalendarForm(forms.ModelForm):
+    class Meta:
+        model = GroupCalendar
         fields = ('title', 'body')
 
     def validate_title(self):
