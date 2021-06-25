5240b4d42abfff6585ca95af5f9d18ce874fdf01
jeongH0 <alswjdgh5095@gmail.com>
2021-06-25 17:22:35 +0900

    feat: add urls.py

--- byurak/accounts/urls.py
+++ byurak/accounts/urls.py
@@ -5,5 +5,11 @@ import accounts.views
 
 urlpatterns = [
     path('', accounts.views.index, name="index"),
+    path('signup/', accounts.views.accounts_signup, name='accounts_signup'),
+    path('login/', accounts.views.accounts_login, name='accounts_login'),
+    path('logout/', accounts.views.accounts_logout, name='accounts_logout'),
+    path("signup_success/", accounts.views.signup_success, name='signup_success'),
+    path("login_success/", accounts.views.login_success, name="login_success"),
+    path("logout_success/", accounts.views.logout_success, name="logout_success"),
 ]
 
