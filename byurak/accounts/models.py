f338cedbc69a7e3bc997123fc29984999c1233b2
oereo <dlstpgns0406@gmail.com>
2021-06-28 12:58:16 +0900

    feat: add ProfileIntroduce migration file and method

--- byurak/accounts/models.py
+++ byurak/accounts/models.py
@@ -116,8 +116,15 @@ class Profile(models.Model):
 class ProfileIntroduce(models.Model):
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE, help_text='유저 프로필')
     image = models.ImageField(upload_to='profile/profile_introduce', blank=True, null=True)
-    description = models.CharField(max_length=511, null=True, blank=True)        
+    description = models.CharField(max_length=511, null=True, blank=True)     
 
+    def __str__(self):
+        return self.profile.user.name
+
+    @property
+    def user_type(self):
+        return self.profile.user_type 
+        
 
 class UserFollow(models.Model):
     user_from = models.ForeignKey(User, on_delete=models.CASCADE, related_name='follow_from_set')
