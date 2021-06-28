739859466839f24d1aaf19eb7c155561d42676c1
oereo <dlstpgns0406@gmail.com>
2021-06-28 15:18:42 +0900

    feat: add profile querysets to json

--- byurak/mapsurfing/views.py
+++ byurak/mapsurfing/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 from django.conf import settings
 from accounts.models import Profile
+import json
 
 def depth1(request):
     profiles = Profile.objects.filter(user_type=Profile.SERVICE_PROVIDER).exclude(address__isnull=True)
-    print(profiles)
+
+    json_profiles_data = json.dumps([profile.to_json() for profile in profiles])
     KAKAO_MAP_KEY = getattr(settings, 'KAKAO_MAP_KEY', 'KAKAO_MAP_KEY')
+
     return render(request, 'surfingmap_depth1.html', {
         'KAKAO_MAP_KEY' : KAKAO_MAP_KEY,
-        "profiles": profiles
+        "profiles": json_profiles_data
         })
 
 
